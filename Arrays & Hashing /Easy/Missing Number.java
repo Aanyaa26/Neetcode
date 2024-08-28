1f0e6f4f7032e9296763860338c5ca345c8517ed
Aanya Sharma <128315909+Aanyaa26@users.noreply.github.com>
2024-08-29 00:53:31 +0530

    Easy
    https://neetcode.io/problems/missing-number

--- Arrays & Hashing /Easy/Missing Number.java
+++ Arrays & Hashing /Easy/Missing Number.java
@@ -1,7 +1,7 @@
 Date: 29.08.24
 
 class Solution {
-    public int missingNumber(int[] nums) {
+    public int missingNumber(int[] nums) { 
         int sum =0;
         int n = nums.length;
         for(int i=0; i<n; i++ ){
