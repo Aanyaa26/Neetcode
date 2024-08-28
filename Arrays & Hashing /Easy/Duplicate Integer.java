2f99fb7583ee4a27cb006f324278d5d73ea9b57d
Aanya Sharma <128315909+Aanyaa26@users.noreply.github.com>
2024-08-29 00:53:57 +0530

    Easy

--- Arrays & Hashing /Easy/Duplicate Integer.java
+++ Arrays & Hashing /Easy/Duplicate Integer.java
@@ -1,7 +1,7 @@
 Date: 29.08.24
 
 class Solution {
-    public boolean hasDuplicate(int[] nums) {
+    public boolean hasDuplicate(int[] nums)  
         Arrays.sort(nums);
         for(int i = 1; i<nums.length; i++){
             if(nums[i-1]==nums[i]) return true;
