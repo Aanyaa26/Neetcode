4bc5e3fd91ac9d8d89ef97e1d27d4237dbe083f0
Aanya Sharma <128315909+Aanyaa26@users.noreply.github.com>
2024-08-29 00:53:03 +0530

    E

---
+++ Arrays & Hashing /Easy/Missing Number.java
@@ -0,0 +1,15 @@
+Date: 29.08.24
+
+class Solution {
+    public int missingNumber(int[] nums) {
+        int sum =0;
+        int n = nums.length;
+        for(int i=0; i<n; i++ ){
+            sum = sum + nums[i];
+                        
+        }int Actual_Sum = (n*(n+1))/2;
+            int missing_number = Actual_Sum - sum;
+            return missing_number;
+        
+    }
+}
