1333079fc52e6d6e56d9646863e487dd8b81bbef
Aanya Sharma <128315909+Aanyaa26@users.noreply.github.com>
2025-02-24 12:26:34 +0530

    Hashmaps - good question

---
+++ NC150/Arrays&Hashing/Valid Anagram.java
@@ -0,0 +1,19 @@
+Date:18.02.24
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length()!=t.length())return false;
+        char [] s1 = s.toCharArray();
+        char [] s2 = t.toCharArray();
+
+        Arrays.sort(s1);
+        Arrays.sort(s2);
+
+        for(int i=0; i<s.length(); i++){
+            if(s1[i]!=s2[i]){
+                return false;
+            }
+        }
+        return true;
+
+    }
+}
