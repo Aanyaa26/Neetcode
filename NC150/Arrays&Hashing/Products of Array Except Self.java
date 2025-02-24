caa2268ba88b6e5415f51f63f93a7b1e67b60f92
Aanya Sharma <128315909+Aanyaa26@users.noreply.github.com>
2025-02-24 12:22:32 +0530

    arrays
    https://neetcode.io/problems/products-of-array-discluding-self

---
+++ NC150/Arrays&Hashing/Products of Array Except Self.java
@@ -0,0 +1,47 @@
+Date: 18.02.24
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int product =1;
+        int count =0;
+        int [] ans = new int [nums.length];
+        for(int i=0;i<nums.length; i++){
+            if(nums[i]==0){
+                count++;
+            }
+
+                                 
+        }
+        for(int i=0; i<nums.length;i++){
+            if(count ==0){
+                product = product*nums[i];
+            }
+
+            else if(count ==1 && nums[i]!=0 ){
+                product = product*nums[i];
+            }
+            else if(count ==1 && nums[i]==0){
+                continue;
+            }
+            else{
+                product =0;
+            }
+        }
+
+        
+        for(int i=0; i<nums.length; i++){
+            if(count==0){
+                ans[i] = product/nums[i];
+            } 
+            else if(count ==1 && nums[i]!=0){
+                ans[i]=0;
+            }   
+            else if(count ==1 && nums[i]==0){
+                ans[i]=product;
+            } 
+            else if(count >1){
+                ans[i]=0;
+            }       
+        }
+        return ans;
+    }
+}  
