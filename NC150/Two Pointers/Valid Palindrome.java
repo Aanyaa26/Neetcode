a5e0afe21ee2ecfaa60fe6e8a8818ef92e57b5e0
Aanya Sharma <128315909+Aanyaa26@users.noreply.github.com>
2025-02-24 21:21:58 +0530

    String, toLowerCase, reverseAll

---
+++ NC150/Two Pointers/Valid Palindrome.java
@@ -0,0 +1,16 @@
+Date: 24.02.24
+class Solution {
+    public boolean isPalindrome(String s) {
+        s = s.toLowerCase().replaceAll("[^a-z0-9]","");
+        int i=0;
+        int j=s.length()-1;
+        while(i<=j){
+            if(s.charAt(i)!=s.charAt(j)){
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+}
