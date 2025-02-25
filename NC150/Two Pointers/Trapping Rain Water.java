ee8acbda0d6ca183ef66eac576342a6e5a8e0101
Aanya Sharma <128315909+Aanyaa26@users.noreply.github.com>
2025-02-25 15:56:27 +0530

    Hard, two pointer

---
+++ NC150/Two Pointers/Trapping Rain Water.java
@@ -0,0 +1,24 @@
+Date: 25.02.25
+public class Solution {
+    public int trap(int[] height) {
+        if (height == null || height.length == 0) {
+            return 0;
+        }
+
+        int l = 0, r = height.length - 1;
+        int leftMax = height[l], rightMax = height[r];
+        int res = 0;
+        while (l < r) {
+            if (leftMax < rightMax) {
+                l++;
+                leftMax = Math.max(leftMax, height[l]);
+                res += leftMax - height[l];
+            } else {
+                r--;
+                rightMax = Math.max(rightMax, height[r]);
+                res += rightMax - height[r];
+            }
+        }
+        return res;
+    }
+}
