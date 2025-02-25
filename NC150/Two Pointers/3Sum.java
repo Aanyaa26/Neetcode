3977be2060166e979c5916bdb06a7dd9fa8c555e
Aanya Sharma <128315909+Aanyaa26@users.noreply.github.com>
2025-02-25 15:57:52 +0530

    Hashset-two pointer - imp

---
+++ NC150/Two Pointers/3Sum.java
@@ -0,0 +1,18 @@
+Date: 25.02.25
+public class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        Set<List<Integer>> res = new HashSet<>();
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                for (int k = j + 1; k < nums.length; k++) {
+                    if (nums[i] + nums[j] + nums[k] == 0) {
+                        List<Integer> tmp = Arrays.asList(nums[i], nums[j], nums[k]);
+                        res.add(tmp);
+                    }
+                }
+            }
+        }
+        return new ArrayList<>(res);
+    }
+}
